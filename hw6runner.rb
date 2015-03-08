72e4d69cbfcd6ce6f8de5ef0448b0754c633b25d
martin823 <martin823@126.com>
2015-03-08 17:14:42 +0800

    Update hw6runner.rb

--- hw6runner.rb
+++ hw6runner.rb
@@ -1,7 +1,7 @@
 # University of Washington, Programming Languages, Homework 6, hw6runner.rb
 
-require_relative './hw6provided'
-require_relative './hw6assignment'
+require_relative './hw6original'
+require_relative './hw6enhanced'
 
 def runTetris
   Tetris.new 
